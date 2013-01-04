8ccb7ed787124a888ce9be51091d33d694f451a4
berto <berto@lobosunidos.org>
2013-01-04 11:08:37 -0500

    added age logic, both cound and timestamp to dash.

--- core/Dash.cpp
+++ core/Dash.cpp
@@ -1,8 +1,10 @@
 #include "Dash.h"
 /////////////////////////////////////////////////
-Dash::Dash() : DashCleaner()
+Dash::Dash(long int ageLimit, long int countLimit) : DashCleaner()
 {
+    this->ageLimit = ageLimit;
 
+    this->countLimit = countLimit;
 }
 /////////////////////////////////////////////////
 Value Dash::get(const std::string& key)
@@ -10,7 +12,17 @@ Value Dash::get(const std::string& key)
     Value result;
 
     kvstoreMutex.lock();
+    
     result = kvstore[key];
+
+    ++kvstore[key].count;
+
+    if ( countLimit != -1 && kvstore[key].count > ageLimit )
+        kvstore.erase(key);
+
+    if ( ageLimit != -1 && (time(NULL) - kvstore[key].timestamp) > ageLimit)
+        kvstore.erase(key);
+    
     kvstoreMutex.unlock();
 
     return result;
@@ -25,7 +37,9 @@ void Dash::put(const std::string& key, const std::string& value)
     if (! kvstore[key].isEmpty() )
 	v = kvstore[key];
 
-    v.count++;
+    // TODO: need separate put and get counts?
+    
+    v.count = 0;
     v.value = value;
     v.setDirty(true);
 
@@ -38,11 +52,15 @@ KVStore Dash::getDirtyValues()
 {
     KVStore result;
 
+    kvstoreMutex.lock();
+
     KVStore::iterator itr = kvstore.begin();
     for (; itr != kvstore.end(); itr++) {
         if (itr->second.isDirty() )
             result.insert(std::pair<std::string, Value>(itr->first, itr->second) );
     }
+
+    kvstoreMutex.unlock();
 
     return result;
 
@@ -52,9 +70,13 @@ std::vector< std::string > Dash::getKeys()
 {
     std::vector<std::string> result;
 
+    kvstoreMutex.lock();
+
     KVStore::iterator itr = kvstore.begin();
     for (; itr != kvstore.end(); itr++)
         result.push_back(itr->first);
+
+    kvstoreMutex.unlock();
 
     return result;
 }
@@ -65,9 +87,130 @@ void Dash::clean(DashCleaner *&dc) {
         dc->clean(NULL);
     }
 }
+
+void Dash::clear()
+{
+    kvstoreMutex.lock();
+
+    kvstore.clear();
+
+    kvstoreMutex.unlock();
+}
+
+KVStore Dash::getValuesByAge(unsigned int age)
+{
+    KVStore result;
+
+    kvstoreMutex.lock();
+
+    KVStore::iterator itr = kvstore.begin();
+    for (; itr != kvstore.end(); itr++) {
+        if (itr->second.timestamp > age)
+            result.insert(std::pair<std::string, Value>(itr->first, itr->second) );
+    }
+
+    kvstoreMutex.unlock();
+
+    return result;
+}
+
+KVStore Dash::getValuesByCount(unsigned int count)
+{
+    KVStore result;
+
+    kvstoreMutex.lock();
+    
+    KVStore::iterator itr = kvstore.begin();
+    for (; itr != kvstore.end(); itr++) {
+        if (itr->second.count > count)
+            result.insert(std::pair<std::string, Value>(itr->first, itr->second) );
+    }
+
+    kvstoreMutex.unlock();
+
+    return result;
+}
+
+void Dash::remove(const std::string& key)
+{
+    kvstoreMutex.lock();
+
+    kvstore.erase(key);
+
+    kvstoreMutex.unlock();    
+}
+
+void Dash::removeValuesByAge(unsigned int age)
+{
+    kvstoreMutex.lock();
+
+    KVStore::iterator itr = kvstore.begin();
+    while (itr != kvstore.end() ) {
+        if (itr->second.timestamp > age) {
+            kvstore.erase(itr);
+
+            // TODO: test this - I'm not sure if this works as expected.
+            itr++;
+        }
+    }
+
+    kvstoreMutex.unlock();
+}
+
+void Dash::removeValuesByCount(unsigned int count)
+{
+    kvstoreMutex.lock();
+
+    KVStore::iterator itr = kvstore.begin();
+    while (itr != kvstore.end() ) {
+        if (itr->second.count > count) {
+            kvstore.erase(itr);
+
+            // TODO: test this - I'm not sure if this works as expected.
+            itr++;
+        }
+    }
+
+    kvstoreMutex.unlock();
+}
+
+long int Dash::getAgeLimit() const
+{
+    return ageLimit;
+}
+
+long int Dash::getCountLimit() const
+{
+    return countLimit;
+}
+
+void Dash::setAgeLimit(long int limit)
+{
+    ageLimit = limit;
+}
+
+void Dash::setCountLimit(long int limit)
+{
+    countLimit = limit;
+}
+
+Value Dash::peek(const std::string& key)
+{
+    Value result;
+    
+    kvstoreMutex.lock();
+
+    result = kvstore[key];
+    
+    kvstoreMutex.unlock();
+
+    return result;
+}
+
+
 /////////////////////////////////////////////////
 Dash::~Dash()
 {
-
+    clear();
 }
 /////////////////////////////////////////////////
--- core/Dash.h
+++ core/Dash.h
@@ -45,21 +45,60 @@ public:
 typedef std::map<std::string, Value> KVStore;
 
 class Dash : public DashCleaner {
-protected:
+private:
     KVStore kvstore;
+    
     mutex kvstoreMutex;
 
+    long int ageLimit;
+    
+    long int countLimit;
+    
+
 public:
-    Dash();
+    Dash(long int age = -1, long int count = -1);
 
     void put(const std::string &key, const std::string &value);
+    
     Value get(const std::string &key);
 
+    /**
+     * This method is an administrator 'get', fetching the value
+     * without affecting the counter.  This is for stats reporting,
+     * and not for fetching.
+     */
+    Value peek(const std::string &key);
+    
+    void remove(const std::string &key);
+
+
     KVStore getDirtyValues();
+
+    KVStore getValuesByAge(unsigned int age);
+
+    KVStore getValuesByCount(unsigned int count);
+
+
+    void removeValuesByAge(unsigned int age);
+
+    void removeValuesByCount(unsigned int count);
+    
+
+    void clear();   
+  
 
     virtual void clean(DashCleaner *&dc);
 
     std::vector<std::string> getKeys();
+
+    void setAgeLimit(long int limit);
+
+    void setCountLimit(long int limit);
+
+    long int getAgeLimit() const;
+
+    long int getCountLimit() const;
+
 
     virtual ~Dash();
 };
